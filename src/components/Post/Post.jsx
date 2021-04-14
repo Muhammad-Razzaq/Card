3501efed66b7c77ca4ae305820dd49f3de739778
Muhammad Razzaq <razzaq123789@gmail.com>
2021-04-14 19:58:44 +0500

    gh-pages

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     },
     media: {
         height: 0,
-        paddingTop: '56.25%', // 16:9
+        paddingTop: '56.25%',
         borderRadius: 9,
         marginTop: 10
     },
